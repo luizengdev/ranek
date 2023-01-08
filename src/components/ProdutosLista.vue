d4839ad74d0b57f615d563e654749486835cde0b
Luiz Almeida <luizengdev@gmail.com>
2023-01-08 08:52:02 -0300

    criacao dos  arquivo helpers.js e services.js, criacao da logica de busca, estilizando a lista de produtos

---
+++ src/components/ProdutosLista.vue
@@ -0,0 +1,105 @@
+<template>
+  <section class="produtos-container">
+    <div v-if="produtos && produtos.length" class="produtos">
+      <div class="produto" v-for="produto in produtos" :key="produto.id">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0]"
+            :alt="produto.fotos[0].titulo"
+          />
+          <p class="preco">{{ produto.preco }}</p>
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p>{{ produto.descricao }}</p>
+        </router-link>
+      </div>
+    </div>
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultados">
+        Busca sem resultados. Tente buscar outro termo.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import { api } from "@/services.js";
+import { serialize } from "@/helpers.js";
+
+export default {
+  name: "ProdutosLista",
+  data() {
+    return {
+      produtos: null,
+      produtosPorPagina: 9,
+    };
+  },
+  computed: {
+    url() {
+      const query = serialize(this.$route.query);
+      return `/produto?_limit=${this.produtosPorPagina}${query}`;
+    },
+  },
+  methods: {
+    getProdutos() {
+      api.get(this.url).then((r) => {
+        this.produtos = r.data;
+      });
+    },
+  },
+  watch: {
+    url() {
+      this.getProdutos();
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
+</script>
+
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <h1>👍 Compre ou Venda ✌</h1>
+    <ProdutosBuscar />
+    <ProdutosLista />
+  </section>
+</template>
+
+<script>
+import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
+import ProdutosLista from "@/components/ProdutosLista.vue";
+
+export default {
+  name: "Home",
+  components: {
+    ProdutosBuscar,
+    ProdutosLista,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
