e6a7579e0c38042d731847feeb45294bbb71c327
Luiz Almeida <luizengdev@gmail.com>
2023-01-11 08:25:43 -0300

    criação do login com vuex

---
+++ src/components/LoginCriar.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>Login Criar</p>
+</template>
+
+<script>
+export default {
+  name: "LoginCriar",
+};
+</script>
+
+<style></style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,41 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { api } from "@/services";
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    login: false,
+    usuario: {
+      id: "",
+      nome: "",
+      email: "",
+      senha: "",
+      cep: "",
+      rua: "",
+      numero: "",
+      bairro: "",
+      cidade: "",
+      estado: "",
+    },
   },
   mutations: {
+    UPDATE_LOGIN(state, payload) {
+      state.login = payload;
+    },
+    UPDATE_USUARIO(state, payload) {
+      state.usuario = payload;
+    },
   },
   actions: {
+    getUsuario(context, payload) {
+      api.get(`/usuario/${payload}`).then((r) => {
+        context.commit("UPDATE_USUARIO", r.data);
+        context.commit("UPDATE_LOGIN", true);
+      });
+    },
   },
-  modules: {
-  }
-})
+  getters: {},
+  modules: {},
+});
