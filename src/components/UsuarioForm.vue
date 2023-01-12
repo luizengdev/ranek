22066906f4116fe75873932363366d0b3e2755f9
Luiz Almeida <luizengdev@gmail.com>
2023-01-12 19:22:19 -0300

    consumindo api viaCep para o campo de cep do formulário

--- src/components/UsuarioForm.vue
+++ src/components/UsuarioForm.vue
@@ -7,7 +7,13 @@
     <label for="senha">Senha</label>
     <input id="senha" name="senha" type="password" v-model="senha" />
     <label for="cep">Cep</label>
-    <input id="cep" name="cep" type="text" v-model="cep" />
+    <input
+      id="cep"
+      name="cep"
+      type="text"
+      v-model="cep"
+      @keyup="preencherCep"
+    />
     <label for="rua">Rua</label>
     <input id="rua" name="rua" type="text" v-model="rua" />
     <label for="numero">Numero</label>
@@ -26,6 +32,7 @@
 
 <script>
 import { mapFields } from "@/helpers.js";
+import { getCep } from "@/services.js";
 
 export default {
   name: "UsuarioForm",
@@ -45,6 +52,19 @@ export default {
       base: "usuario",
       mutation: "UPDATE_USUARIO",
     }),
+  },
+  methods: {
+    preencherCep() {
+      const cep = this.cep.replace(/\D/g, "");
+      if (cep.length === 8) {
+        getCep(cep).then((r) => {
+          this.rua = r.data.logradouro;
+          this.bairro = r.data.bairro;
+          this.estado = r.data.uf;
+          this.cidadde = r.data.localidade;
+        });
+      }
+    },
   },
 };
 </script>
