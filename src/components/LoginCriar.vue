90f709c15d87e4e25acfddbaaf900add72c10296
Luiz Almeida <luizengdev@gmail.com>
2023-01-12 19:49:33 -0300

    criação da função de post do usuario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -6,7 +6,9 @@
         Criar Conta
       </button>
       <UsuarioForm v-else>
-        <button class="btn btn-form">Criar Usuário</button>
+        <button class="btn btn-form" @click.prevent="criarUsuario">
+          Criar Usuário
+        </button>
       </UsuarioForm>
     </transition>
   </section>
@@ -24,6 +26,20 @@ export default {
     return {
       criar: false,
     };
+  },
+  methods: {
+    async criarUsuario() {
+      try {
+        await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
+        await this.$store.dispatch(
+          "getUsuario",
+          this.$store.state.usuario.email
+        );
+        this.$router.push({ name: "usuario" });
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
